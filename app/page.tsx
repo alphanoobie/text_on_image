601c92bac19081e70beaab280add5636a61018a3
alphanoobie <hussaingadi.hg@gmail.com>
2023-09-04 12:37:19 +0530

    resiazable input box

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,52 @@
 "use client";
-import { log } from "console";
 import Image from "next/image";
 import { useState } from "react";
+import { ResizableBox } from "react-resizable";
+import "react-resizable/css/styles.css";
 
 export default function Home() {
   const [image, setImage] = useState(null);
+  const [fontSize, setFontSize] = useState(20);
+
   const getImage = async () => {
-    const response: any = await fetch("/api/getimage");
+    const response = await fetch("/api/getimage");
     const data = await response.json();
     if (data.ok) {
       setImage(data.message);
     }
   };
 
+  const handleResize = (event: any, { node, size, handle }: any) => {
+    console.log(size);
+  };
+
   return (
     <div>
-      <button onClick={getImage}>get image</button>
+      <button onClick={getImage}>Get Image</button>
       <div className="w-[100vw] flex justify-center">
         {image && (
-          <div>
-            <Image src={image} height={500} width={500} alt="" />
+          <div className="w-[500px] h-[500px] relative">
+            <Image
+              src={image}
+              layout="fill" // Fill the parent container
+              alt=""
+              className="relative"
+            />
+            <ResizableBox
+              height={50}
+              width={100}
+              maxConstraints={[500, 500]}
+              resizeHandles={["n", "s", "e", "w", "ne", "nw", "se", "sw"]}
+              className="bg-white opacity-50"
+              onResize={handleResize}
+            >
+              <input
+                style={{ fontSize: fontSize }}
+                className={`h-full w-full`}
+                width={100}
+                height={100}
+              />
+            </ResizableBox>
           </div>
         )}
       </div>
